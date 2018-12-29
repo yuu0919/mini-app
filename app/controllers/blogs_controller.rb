8f185a8a295d2e5410037aa8508ca59c9a2ae2ad
yuu0919 <yw3055.jenesice@gmail.com>
2018-12-29 17:05:44 +0900

    Midst commit

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,30 @@
 class BlogsController < ApplicationController
+
   def index
+    @blogs = Blog.order('created_at DESC').limit(5)
+  end
+
+  def new
+    @blogs = Blog.new
+  end
+
+  def create
+    @blogs = Blog.create(blog_params)
+    redirect_to :blogs, action: :index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  private
+
+  def blog_params
+    params.require(:text).permit(:text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'blogs#index'
+  resources :blogs
+  resources :users, only: [:show]
 end
